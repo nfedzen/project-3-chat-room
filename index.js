19f3223eae64821c5757a6579721100898c9ac94
Nathan Fedzen <nfedzen@hotmail.com>
2021-02-17 15:54:12 -0700

    stuck on broadcast

--- index.js
+++ index.js
@@ -2,6 +2,8 @@ const app = require('express')();
 const http = require('http').Server(app);
 const io = require('socket.io')(http);
 
+
+
 app.get('/', (req, res) => {
   res.sendFile(__dirname + '/index.html');
 });
---
+++ server.js
@@ -0,0 +1,12 @@
+const io = require("socket.io")(3000, {
+  cors: {
+    origin: "http://localhost:3001",
+  }
+});
+
+io.on('connection', socket =>{
+  socket.on('send-chat-message', message => {
+    socket.broadcast.emit('chat-message', message) 
+  })
+})
+
